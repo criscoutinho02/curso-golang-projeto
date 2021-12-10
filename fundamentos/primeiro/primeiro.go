72d742ac086acf6f45b492c413c1949023c263df
Cristina Coutinho <cris.coutinho02@gmail.com>
2021-12-09 23:32:22 -0300

    GO GOLANG Curso Cod3r : Go (Golang): Explorando a Linguagem do Google

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,21 @@
+//Programas executavris iniciam pelo pacote main
+package main
+
+/*
+	Os códigos em gosao organizados em pacotes
+	e para usá-los é necessário declarar um ou vários imports
+*/
+
+import "fmt"
+
+//A porta de entrada de um programa Go é a função main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+
+	/*
+		Priorize codigo limpo claro legivel e faça comentarios que agrega valor
+		Evite comentários óbvios
+		Durante o curso abuse dos comentários
+	*/
+}
