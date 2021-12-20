096862b9d2980ad51511a802c90b3d94a913be9f
Cristina Coutinho <cris.coutinho02@gmail.com>
2021-12-20 17:48:57 -0300

    fim modulo pacotes

---
+++ pacotes/usandoLib/usandolib.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/criscoutinho02/area"
+	"github.com/criscoutinho02/area/abc"
+)
+
+func main() {
+	fmt.Println(area.Circ(2.0))
+	fmt.Println(area.Rect(2.0, 4.0))
+	abc.Epa()
+}
